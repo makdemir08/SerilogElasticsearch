039e6b600a4b26500b2e13c72fced79c50fcf2c4
Mustafa AKDEMİR <makdemir08@hotmail.com>
2023-11-08 20:26:27 +0300

    first working example

---
+++ SerilogElasticSearch/Program.cs
@@ -0,0 +1,46 @@
+﻿using Nest;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var node = new Uri("http://localhost:9200");
+
+// Elasticsearch bağlantısı oluşturun
+var settings = new ConnectionSettings(node)
+    .DefaultIndex("log") // Kullandığınız index adını belirtin
+    .DisableDirectStreaming()
+    .PrettyJson();
+
+var client = new ElasticClient(settings);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions
+    {
+        IndexFormat = "log-{0:yyyy.MM.dd}", // İndeks formatını belirtin, tarih bilgisi ile indeksler oluşturulur
+    })
+    .CreateLogger();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+Log.Information("deneme");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
